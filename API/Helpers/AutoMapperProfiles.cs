84d66ade15f559a6b456c7bc146e46ba0693e723
RobertKochanski <kochanskirobert1999@gmail.com>
2022-09-16 11:08:16 +0200

    End of section 10

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -17,6 +17,7 @@ namespace API.Helpers
                 .ForMember(x => x.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain == true).Url))
                 .ForMember(x => x.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
             CreateMap<Photo, PhotoDto>();
+            CreateMap<MemberUpdateDto, AppUser>();
         }
     }
 }
